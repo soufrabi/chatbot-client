b01a624605dc99138814b93a617fbeb0b86df536
Anirban Dey <foss@soufrabi.com>
2023-11-25 00:12:05 +0530

    create script

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,15 +1,52 @@
+#!/bin/sh
+
+show_help(){
+
+	cat << EOF
+linux.sh
+
+Choose one of the available commands:
+	build-deb
+	build-flatpak
+	setup
+	deps
+	help | --help | -h
+	
+EOF
+
+
+}
+
+
+if [ $# -eq 0 ]; then
+	show_help
+	exit
+fi
+
+
 pkgname="chatbot-client"
 _pkgname="chatbot-client"
 command="chatbot-client"
 build_dir="build_binary"
 
-prepare :
-	npm install
-	npm run build
+install_dependencies() {
+	echo "Install dependencies in Linux"
+
+	# sudo apt update && sudo apt install -y libgl1 ffmpeg libsm6 libxext6 libegl1 '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
 
 
-deb:
-	echo ${build_dir}
+}
+
+
+setup_env() {
+    npm install
+    npm run build
+
+}
+
+build_deb() {
+
+    echo ${build_dir}
 	rm -rf ${build_dir}
 	mkdir -pv "${build_dir}/${_pkgname}"
 	mkdir -pv "${build_dir}/${_pkgname}/usr"
@@ -21,8 +58,10 @@ deb:
 	cd ${build_dir} && dpkg-deb --build ${_pkgname}
 
 
-flatpak:
-	ar -x chatbot-client.deb
+}
+
+build_flatpak() {
+    ar -x chatbot-client.deb
 	rm -vf chatbot-client.deb control.tar.xz debian-binary
 	tar -xf data.tar.*
 	rm -vf data.tar.xz
@@ -33,3 +72,44 @@ flatpak:
 	desktop-file-edit --set-key=Exec --set-value="/app/bin/chatbot-client" /app/share/applications/chatbot-client.desktop
 	desktop-file-edit --set-key=Icon --set-value="io.github.anirbandey1.ChatbotClient" /app/share/applications/chatbot-client.desktop
 
+}
+
+
+
+
+
+main() {
+
+	case "$1" in 
+		(build-deb)
+			shift
+			build_deb "$@"
+			;;
+		(build-flatpak)
+			shift
+			build_flatpak "$@"
+			;;
+		(deps)
+			shift
+			install_dependencies "$@"
+			;;
+		(setup)
+			shift
+			setup_env "$@"
+			;;
+		(help | --help | -h)
+			show_help 
+			exit 0 
+			;;
+		(*)
+			printf >&2 "Error: invalid command\n"
+			show_help
+			exit 1
+			;;
+
+	esac
+	
+
+}
+
+main "$@"
