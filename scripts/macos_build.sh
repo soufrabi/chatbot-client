863205450acb8275349ba88807671980a80ff98c
Anirban Dey <code@soufrabi.com>
2023-12-10 11:56:57 +0530

    package binary

--- scripts/macos_build.sh
+++ scripts/macos_build.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
-build_dir="build-macos-$(uname -p)"
+printf "Build on MacOS"
+architecture="$(uname -p)"
 
+if [ "$architecture" = "i386" ] ; then 
+    architecture="aarch64"
+fi
+
+build_dir="build-macos-${architecture}"
 echo "$build_dir"
 
-printf "Build on MacOS"
+build_nativifier_binary() {
+    npm install
+    npm build
+
+}
+
+package_binary() {
+    mkdir "$build_dir"
+
+}
+
+build_main() {
+
+    build_nativifier_binary
+    package_binary
+}
+
+build_main
+
 
 
